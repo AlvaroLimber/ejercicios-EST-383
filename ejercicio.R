839d7b69661eb42b14c59791a518e7325cce37d8
Alvaro Limber Chirino Gutierrez <alvaro.rqsbo@gmail.com>
2020-04-22 16:32:37 -0400

    Subiendo a github

---
+++ ejercicio.R
@@ -0,0 +1,5 @@
+#6. Empleando la ENDSA muestre por año y departamento 
+#el porcentaje de personas que fuman
+#7. Utilizando la base de datos del COVID-19 genere un gráfico de evolución de contagios, muertes y recuperados. Use los gráficos de origen
+#8. Utilizando la base de datos del COVID-19 genere un gráfico de evolución de contagios, muertes y recuperados. Use ggplot
+rm(list = ls())
